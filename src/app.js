4fabeb89b6aeda4da095bd48bed21db3e15d7cf6
ritu11x <vishwakarmaritu011@gmail.com>
2026-05-14 18:30:05 +0530

    routers

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 
+
 const app = express()
 
 app.use(cors({
@@ -19,5 +20,10 @@ app.use(express.urlencoded({
 app.use(express.static("public"))
 app.use(cookieParser())
 
+//routes imports 
+import userRouter from './routes/user.routes.js'
 
+//routes declaration
+app.use('/api/v1/users', userRouter)
+// http://localhost:8000/users/register
 export { app } 
